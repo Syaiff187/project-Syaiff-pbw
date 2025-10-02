77dc59965b8fbf2503e515d464a84af3ed6e8d07
Syaiff187 <koclokarif5@gmail.com>
2025-10-02 22:07:28 +0700

    Templating Engine

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -1,41 +1,8 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('layoutt.main')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="style.css">
-
-    <title>Profil Mahasiswa</title>
-  </head>
-  <body>
-    <!-- Navigasi -->
-    <ul class="nav justify-content-center bg-light shadow-sm p-2">
-      <li class="nav-item">
-        <a class="nav-link" href="/">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="profile">Profil</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/berita">Berita</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/kontak">Kontak</a>
-      </li>
-    </ul>
-
-    <!-- Konten -->
-    <div class="container text-center mt-5">
-      <div class="profile-card">
-        <h1 class="nama">Achmad Syaifulloh</h1>
-        <p class="nim">NIM : 13242420020</p>
-        <p class="prodi">Prodi : Teknologi Informasi</p>
-      </div>
-    </div>
-  </body>
-</html>
+@section('content')
+  <h1>Kontak</h1>
+  
+  <h3>{{ $gmail }}</h3>
+  
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,46 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "home",
+    ]);
 });
 
 Route::get('/profile', function () {
-    return view('profile');
+    return view('profile', [
+        "title" => "profile",
+        "nama" => "Ach Syaif", 
+        "nohp" => "08684637390",
+        "foto" => "img/syaif.webp",
+    ]);
 });
 
 Route::get('/berita', function () {
-    return view('berita');
+
+    $data_berita = [
+        [
+            "judul" => "Festival Budaya Nusantara 2025 Resmi Dibuka di Yogyakarta",
+            "penulis" => "Syaif",
+            "konten" => "Festival Budaya Nusantara 2025 resmi dibuka pada hari Rabu, 1 Oktober 2025, di Alun-Alun Utara Yogyakarta. Acara ini menghadirkan lebih dari 200 peserta dari berbagai daerah di Indonesia dengan menampilkan seni tari, musik tradisional, hingga kuliner khas. Ribuan pengunjung memadati area festival sejak pagi hari. Pemerintah daerah berharap acara tahunan ini dapat memperkuat persatuan bangsa sekaligus mempromosikan potensi budaya Indonesia ke mancanegara.",
+        ],
+        [
+            "judul" => "Peluncuran Satelit Merah Putih 3 Sukses Dilakukan",
+            "penulis" => "parel",
+            "konten" => "Indonesia kembali mencatat sejarah penting di bidang teknologi antariksa setelah sukses meluncurkan Satelit Merah Putih 3 pada Selasa, 30 September 2025. Satelit yang diluncurkan dari Kennedy Space Center, Amerika Serikat, ini akan memperkuat jaringan komunikasi nasional sekaligus mendukung pengembangan internet di daerah terpencil. Menteri Komunikasi menyatakan bahwa satelit ini menjadi langkah strategis dalam mewujudkan pemerataan akses digital di seluruh wilayah Indonesia.",
+        ],
+    ];
+
+    return view('berita',[
+        "title" => "Berita",
+        "beritas" => $data_berita,
+    ]);
 });
 
 Route::get('/kontak', function () {
-    return view('kontak');
+    return view('kontak', [
+        "title" => "kontak",
+        "gmail" => "koclokarif5@gmail.com"
+        
+    ]);
 });
 
